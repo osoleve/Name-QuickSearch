876d1e74f725dfd542a369416cee0966e6a49dee
Oso <andrew.levenson@gmail.com>
2021-07-19 20:32:31 -0400

    Configured brittany for formatting, aligning more with style guide now.

--- src/QuickSearch/Internal/Matcher.hs
+++ src/QuickSearch/Internal/Matcher.hs
@@ -1,28 +1,38 @@
 {-# LANGUAGE ViewPatterns #-}
 
 module QuickSearch.Internal.Matcher
-  ( scoreMatches
-  , Score
-  , Scorer
-  , Match(..)
-  , matchScore
-  , matchEntry
-  , QuickSearch(QuickSearch)
-  )
-where
+    ( scoreMatches
+    , Score
+    , Scorer
+    , Match(..)
+    , matchScore
+    , matchEntry
+    , quickSearchEntries
+    , quickSearchTokenFilter
+    , QuickSearch(QuickSearch)
+    ) where
 
-import           Control.Arrow               (Arrow ((&&&)))
-import           Data.Bifunctor              (Bifunctor (first))
-import qualified Data.HashMap.Lazy           as HMap
-import qualified Data.HashSet                as HSet
-import           Data.Hashable               (Hashable)
-import           Data.List                   (sortBy)
-import           Data.Ord                    (Down (Down), comparing)
-import           Data.Ratio                  (Ratio, denominator, numerator)
-import qualified Data.Text                   as T
+import           Control.Arrow                  ( Arrow((&&&)) )
+import           Data.Bifunctor                 ( Bifunctor(first) )
+import qualified Data.HashMap.Lazy             as HMap
+import qualified Data.HashSet                  as HSet
+import           Data.Hashable                  ( Hashable )
+import           Data.List                      ( sortBy )
+import           Data.Ord                       ( Down(Down)
+                                                , comparing
+                                                )
+import           Data.Ratio                     ( Ratio
+                                                , denominator
+                                                , numerator
+                                                )
+import qualified Data.Text                     as T
 
-import           QuickSearch.Internal.Filter (Entry (..), Token, entryName,
-                                              entryUID, getSearchPartition)
+import           QuickSearch.Internal.Filter    ( Entry(..)
+                                                , Token
+                                                , entryName
+                                                , entryUID
+                                                , getSearchPartition
+                                                )
 
 type Score = Int
 type Scorer = (T.Text -> T.Text -> Ratio Int)
@@ -32,7 +42,7 @@ newtype Match score entry = Match (score, entry)
   deriving (Show)
 
 -- | Score accessor for Match
-matchScore :: Match Score (Entry name uid)  -> Score
+matchScore :: Match Score (Entry name uid) -> Score
 matchScore (Match (score, _)) = score
 
 -- | Entry accessor for Match
@@ -48,50 +58,47 @@ newtype QuickSearch uid =
   deriving (Show)
 
 -- | [Entry name uid] accessor for QuickSearch
-quickSearchEntries
-  :: (Hashable uid, Eq uid) => QuickSearch uid -> [Entry T.Text uid]
+quickSearchEntries :: (Hashable uid, Eq uid) => QuickSearch uid -> [Entry T.Text uid]
 quickSearchEntries (QuickSearch (entries, _)) = entries
 
 
 -- | tokenFilter accessor for QuickSearch
 quickSearchTokenFilter
-  :: (Hashable uid, Eq uid)
-  => QuickSearch uid
-  -> HMap.HashMap Token (HSet.HashSet uid)
-quickSearchTokenFilter (QuickSearch (_, tokenFilter))= tokenFilter
+    :: (Hashable uid, Eq uid) => QuickSearch uid -> HMap.HashMap Token (HSet.HashSet uid)
+quickSearchTokenFilter (QuickSearch (_, tokenFilter)) = tokenFilter
 
 {- | Given a string to search, a QuickSearch object, and a similarity function,
    returns potential matches contained in the QuickSearch filters and their
    associated scores, in descending order by score.
 -}
 scoreMatches
-  :: (Hashable uid, Eq uid)
-  => T.Text  -- ^ Name or other string to be searched
-  -> QuickSearch uid  -- ^ The QuickSearch object to be used
-  -> Scorer  -- ^ A string distance function of type (Text -> Text -> Ratio Int)
-  -> [Match Score (Entry T.Text uid)]  -- ^ A list of possible matches 
+    :: (Hashable uid, Eq uid)
+    => T.Text  -- ^ Name or other string to be searched
+    -> QuickSearch uid  -- ^ The QuickSearch object to be used
+    -> Scorer  -- ^ A string distance function of type (Text -> Text -> Ratio Int)
+    -> [Match Score (Entry T.Text uid)]  -- ^ A list of possible matches
 scoreMatches (T.toCaseFold -> entry) qs scorer =
-  let searchSpace = pruneSearchSpace entry qs
-      searchSpace' = map (first T.toCaseFold) searchSpace
-      scoreEntry = toPercent . scorer entry . entryName
-      results     = map (Match . (scoreEntry &&& id)) searchSpace
-  in  sortBy (comparing (Down . matchScore)) results
+    let searchSpace  = pruneSearchSpace entry qs
+        searchSpace' = map (first T.toCaseFold) searchSpace
+        scoreEntry   = toPercent . scorer entry . entryName
+        results      = map (Match . (scoreEntry &&& id)) searchSpace'
+    in  sortBy (comparing (Down . matchScore)) results
 -- ^ Ignore the linter here, this is a performance thing
 
 {- | Given a string and a QuickSearch object, return the list of entries
    from within the QuickSearch that share a full token with the target string
 -}
 pruneSearchSpace
-  :: (Hashable uid, Eq uid)
-  => T.Text  -- ^ Name or other string to be searched
-  -> QuickSearch uid  -- ^ The QuickSearch object to be used
-  -> [Entry T.Text uid]  -- ^ A list of strings to search through and their UIDs
+    :: (Hashable uid, Eq uid)
+    => T.Text  -- ^ Name or other string to be searched
+    -> QuickSearch uid  -- ^ The QuickSearch object to be used
+    -> [Entry T.Text uid]  -- ^ A list of strings to search through and their UIDs
 pruneSearchSpace entry (QuickSearch (entries, tokenFilter)) =
-  let uidPartition = getSearchPartition entry tokenFilter
-  in  filter ((`HSet.member` uidPartition) . entryUID) entries
+    let uidPartition = getSearchPartition entry tokenFilter
+    in  filter ((`HSet.member` uidPartition) . entryUID) entries
 
 toPercent :: Ratio Int -> Int
-toPercent r = floor $ (num / denom) * 100
- where
-  ratioToIntPair = fromIntegral . numerator &&& fromIntegral . denominator
-  (num, denom)   = ratioToIntPair r
+toPercent r = floor $ (num / denom) * (100 :: Double)
+  where
+    ratioToIntPair = fromIntegral . numerator &&& fromIntegral . denominator
+    (num, denom)   = ratioToIntPair r
--- src/QuickSearch/String/OneShot.hs
+++ src/QuickSearch/String/OneShot.hs
@@ -1,52 +1,56 @@
 module QuickSearch.String.OneShot
-  ( oneShot
-  , oneShotTopNMatches
-  , oneShotMatchesWithThreshold
-  )
-where
+    ( oneShot
+    , oneShotTopNMatches
+    , oneShotMatchesWithThreshold
+    ) where
 
-import           Data.Hashable      (Hashable)
-import qualified Data.Text          as T
-import           Data.Text.Metrics  (damerauLevenshteinNorm, jaro, jaroWinkler)
+import           Data.Hashable                  ( Hashable )
+import           Data.Text.Metrics              ( )
 
-import           QuickSearch.String (Entry (..), Match, QuickSearch, Score,
-                                     Scorer, buildQuickSearch,
-                                     matchesWithThreshold, topNMatches)
+import           QuickSearch.String             ( Entry(..)
+                                                , Match
+                                                , QuickSearch
+                                                , Score
+                                                , Scorer
+                                                , buildQuickSearch
+                                                , matchesWithThreshold
+                                                , topNMatches
+                                                )
 
 {- | Turn a match retrieval function into a one-shot batch function.
    Instead of creating a QuickSearch for reuse, this creates it in the
    background and discards it when done.
 -}
 oneShot
-  :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
-  => (  QuickSearch uid2
-     -> Int
-     -> Scorer
-     -> String
-     -> [Match Score (Entry String uid2)]
-     )
+    :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
+    => (  QuickSearch uid2
+       -> Int
+       -> Scorer
+       -> String
+       -> [Match Score (Entry String uid2)]
+       )
   -- ^ Match retrieval function to be converted into a one-shot
-  -> Int  -- ^ The reference number for the match retrieval function.
-  -> [(String, uid1)]  -- ^ List of entries to be processed
-  -> [(String, uid2)]  -- ^ List of entries making up the search space
-  -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
-  -> [(Entry String uid1, [Match Score (Entry String uid2)])]
+    -> Int  -- ^ The reference number for the match retrieval function.
+    -> [(String, uid1)]  -- ^ List of entries to be processed
+    -> [(String, uid2)]  -- ^ List of entries making up the search space
+    -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
+    -> [(Entry String uid1, [Match Score (Entry String uid2)])]
     -- ^ List of entries and their matches.
 oneShot f n entries targets scorer =
-  let qs      = buildQuickSearch targets
-      results = map (f qs n scorer . fst) entries
-  in  zip (map Entry entries) results
+    let qs      = buildQuickSearch targets
+        results = map (f qs n scorer . fst) entries
+    in  zip (map Entry entries) results
 
 {- | One-shot version of topNMatches. Builds the QuickSearch in the background
    and discards it when finished.
 -}
 oneShotTopNMatches
-  :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
-  => Int  -- ^ N: Number of matches to return
-  -> [(String, uid1)]  -- ^ List of entries to be processed
-  -> [(String, uid2)]  -- ^ List of entries making up the search space
-  -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
-  -> [(Entry String uid1, [Match Score (Entry String uid2)])]
+    :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
+    => Int  -- ^ N: Number of matches to return
+    -> [(String, uid1)]  -- ^ List of entries to be processed
+    -> [(String, uid2)]  -- ^ List of entries making up the search space
+    -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
+    -> [(Entry String uid1, [Match Score (Entry String uid2)])]
   -- ^ List of entries and up to N of the best matches.
 oneShotTopNMatches = oneShot topNMatches
 
@@ -54,11 +58,11 @@ oneShotTopNMatches = oneShot topNMatches
    the background and discards it when finished.
 -}
 oneShotMatchesWithThreshold
-  :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
-  => Int  -- ^ Score threshold above which to return matches
-  -> [(String, uid1)]  -- ^ List of entries to be processed
-  -> [(String, uid2)]  -- ^ List of entries making up the search space
-  -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
-  -> [(Entry String uid1, [Match Score (Entry String uid2)])]
+    :: (Hashable uid1, Eq uid1, Hashable uid2, Eq uid2)
+    => Int  -- ^ Score threshold above which to return matches
+    -> [(String, uid1)]  -- ^ List of entries to be processed
+    -> [(String, uid2)]  -- ^ List of entries making up the search space
+    -> Scorer  -- ^ Similarity function with type (Text -> Text -> Ratio Int)
+    -> [(Entry String uid1, [Match Score (Entry String uid2)])]
   -- ^ List of entries and their matches above the score threshold.
 oneShotMatchesWithThreshold = oneShot matchesWithThreshold
